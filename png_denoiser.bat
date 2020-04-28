3405a5ad224e84463c3fe65477767a5864f3a1b0
Jackjt8 <jackjt8@gmail.com>
2020-04-28 16:55:42 +0100

    Update png_denoiser.bat
    png denoiser supports albedo and normal maps with typical error handling

--- png_denoiser.bat
+++ png_denoiser.bat
@@ -31,27 +31,72 @@ IF "%1"=="" GOTO Break
 		ECHO ===
 		REM check input
 		ECHO %~n1 | FINDSTR /C:".albedo" >nul && (
-			ECHO ### ERROR Wrong input - expected hdr but got albedo
+			ECHO ### ERROR Wrong input - expected png but got albedo
 			GOTO Break
 		)
 		ECHO %~n1 | FINDSTR /C:".normal" >nul && (
-			ECHO ### ERROR Wrong input - expected hdr but got normal
+			ECHO ### ERROR Wrong input - expected png but got normal
 			GOTO Break
 		)
 		ECHO %~n1 | FINDSTR /C:".denoised" >nul && (
-			ECHO ### ERROR Wrong input - expected hdr but got a denoised image
+			ECHO ### ERROR Wrong input - expected png but got a denoised image
 			GOTO Break
 		)
 		REM check input done
 		
-		%dn_path% -i "%1" -o "%~n1_dn%~x1"
+		DIR /b /a-d %~dp1%~n1*%~x1 > %temp%\pngFileList
+		FIND %temp%\pngFileList ".albedo.">nul
+		if errorlevel 1 (
+			REM missing albedo
+			FIND %temp%\pngFileList ".normal.">nul
+			if errorlevel 1 (
+				REM missing normal
+				GOTO denoise_h
+			) else (
+				REM found normal
+				GOTO denoise_hn			
+			)
+		) else (
+			REM found albedo
+			FIND %temp%\pngFileList ".normal.">nul
+			if errorlevel 1 (
+				REM missing normal
+				GOTO denoise_ha
+			) else (
+				REM found normal
+				GOTO denoise_han
+			)
+		)
+		REM %dn_path% -i "%1" -o "%~n1_dn%~x1"
 		REM %dn_path2% -i %1 -o %~n1_dn%~x1
+		
+		:postdenoise
+		DEL %temp%\pngFileList
 		SHIFT
 	IF NOT "%1"=="" (GOTO loop) ELSE (GOTO Break)
 REM you should not end up here. If the following message shows I messed up.
 ECHO ### ERROR missed postdenoise and loop/break point
 GOTO Break	
 
+:denoise_h
+ECHO Found png
+%dn_path% -i "%1" -o "%~dp1%~n1.denoised%~x1"
+GOTO postdenoise
+
+:denoise_ha
+ECHO Found png and Albedo
+%dn_path% -i "%1" -o "%~dp1%~n1.denoised%~x1" -a "%~dp1%~n1.albedo%~x1"
+GOTO postdenoise
+
+:denoise_hn
+ECHO ### ERROR %~1 Found normal but missing albedo?
+PAUSE
+GOTO Break	
+
+:denoise_han
+ECHO Found png, Albedo, and Normal
+%dn_path% -i "%1" -o "%~dp1%~n1.denoised%~x1" -a "%~dp1%~n1.albedo%~x1" -n "%~dp1%~n1.normal%~x1"
+GOTO postdenoise
 
 :PNGerror
 ECHO ### ERROR %1
